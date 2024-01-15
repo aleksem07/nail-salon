1f7af2784788067d00a662aa97260a83ef6e6513
aleksem07 <aleksem07@gmail.com>
2024-01-16 02:41:40 +0300

    feat: add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      
+      <Image
+        src='/images/next.svg'
+        alt='Next.js Logo'
+        width={180}
+        height={37}
+        priority
+      />
     </>
-  )
+  );
 }
