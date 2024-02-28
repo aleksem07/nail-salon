82484b37e53e74a7e6d00397305e10317382282d
aleksem07 <aleksem07@gmail.com>
2024-02-28 21:33:14 +0300

    feat: add map

--- src/components/contacts/contacts.tsx
+++ src/components/contacts/contacts.tsx
@@ -1,14 +1,17 @@
 import styles from '@/UI/sass/container.module.scss';
 import { AppRoutes } from '@/common/routes';
+import MapComponent from '@/components/map/map';
 
 const Contacts = () => {
   return (
     <>
-      <section
-        id={`${AppRoutes.CONTACTS.slice(1)}`}
-        className='min-h-[100vh] bg-red-100'
-      >
-        <div className={`${styles.container}`}>Contacts</div>
+      <section id={`${AppRoutes.CONTACTS.slice(1)}`} className=' bg-red-100'>
+        <div className={`${styles.container}`}>
+          <div className='mb-6 mt-10 text-center text-base font-semibold uppercase leading-6 tracking-widest text-slate-500'>
+            Контакты
+          </div>
+          <MapComponent />
+        </div>
       </section>
     </>
   );
